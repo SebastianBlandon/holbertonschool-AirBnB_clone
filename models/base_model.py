1638ac726a2cdcd78e8ae8405c1955cd79d912af
SebastianBlandon <blandonodz@gmail.com>
2022-07-01 12:50:06 -0500

    [UPDATED] base_model.py file

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
     class BaseModel that defines all common attributes/methods for other classes
 """
 from uuid import uuid4
-from datetime import date
+from datetime import datetime
 
 
 class BaseModel():
@@ -11,16 +11,18 @@ class BaseModel():
     def __init__(self):
         """ Method __init__ initialize the all attibutes """
         self.id = str(uuid4())
-        self.created_at = date.today()
-        self.updated_at = date.today()
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
 
     def __str__(self):
         return "[{}] ({}) {}"\
-                .format(self.__name__, self.id, self.__dict__)
+                .format(BaseModel.__name__, self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = date.today()
+        self.updated_at = datetime.today()
 
     def to_dict(self):
+        self.created_at = str(self.created_at.isoformat("T"))
+        self.updated_at = str(self.updated_at.isoformat("T"))
         return self.__dict__
 
