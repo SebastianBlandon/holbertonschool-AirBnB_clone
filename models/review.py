de7d979d2c97415e5aff0b68d21f0ba740fec6db
SebastianBlandon <blandonodz@gmail.com>
2022-07-06 18:10:31 -0500

    [ADDED] amenity.py, city.py, place.py, revies.py, state.py

---
+++ models/review.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    class Place that inherits from BaseModel:
+"""
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+    """ class Place """
+    place_id = ""
+    user_id = ""
+    text = ""
