c44f4670d7a0924b49faf047b0233fb683f11168
rmbodegalibre <rmbodegalibre@gmail.com>
2022-07-06 23:33:30 -0500

    test_console.py 07

--- tests/test_console.py
+++ tests/test_console.py
@@ -30,10 +30,7 @@ class TestConsole(unittest.TestCase):
    """Check for docstring existance"""
     def test_docstrings_in_console(self):
         """Test docstrings exist in console.py"""
-        self.assertTrue(len(console.__doc__) >= 1)
-
-    def test_docstrings_in_test_console(self):
-        """Test docstrings exist in test_console.py"""
+        self.assertTrue(len(console.__doc__) >= 1)test_console.py"""
         self.assertTrue(len(self.__doc__) >= 1)
 
     """Test command interpreter outputs"""
