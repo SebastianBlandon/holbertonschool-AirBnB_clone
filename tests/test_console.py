da124984a9a1360a839e22e54a36c4f62a33cc48
rmbodegalibre <rmbodegalibre@gmail.com>
2022-07-06 23:36:34 -0500

    test_console.py 12

--- tests/test_console.py
+++ tests/test_console.py
@@ -17,7 +17,7 @@ class TestConsole(unittest.TestCase):
     def setUpClass(self):
         """Setting of test"""
         self.typing = console.HBNBCommand()
-"""
+
     @classmethod
     def tearDownClass(self):
         """ Remove file.json temp """
@@ -25,15 +25,6 @@ class TestConsole(unittest.TestCase):
             os.remove("file.json")
         except:
             pass
-"""
-
-   """Check for docstring existance"""
-    def test_docstrings_in_console(self):
-                self.assertTrue(len(console.__doc__) >= 1)test_console.py"""
-        self.assertTrue(len(self.__doc__) 
-
-
-    """Test command interpreter outputs"""
 
 if __name__ == "__main__":
     unittest.main()
