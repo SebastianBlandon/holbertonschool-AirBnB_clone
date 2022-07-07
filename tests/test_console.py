3d331416989ade7bab7d6e32ad946ef08c36a932
rmbodegalibre <rmbodegalibre@gmail.com>
2022-07-06 22:47:36 -0500

    test_console.py 01

---
+++ tests/test_console.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Unittest - For test console AirBnB 
+"""
+import unittest
+import os
+import json
+import console
+import tests
+from models.base_model import BaseModel
+
+class TestConsole(unittest.TestCase):
+    """ Class designed for test """
+    
+if __name__ == "__main__":
+    unittest.main()
